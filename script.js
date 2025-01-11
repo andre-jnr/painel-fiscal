991440f50038b706e17495820eb0647f91f3cae7
andre-jnr <andrejunior.pessoal@gmail.com>
2025-01-11 09:40:14 -0400

    Texto para NF de produtor rural e devolução parcial para copiar e colar
    Implementado um botão para copiar e colar nos dados adicionais nas notas fiscais de entrada de produtor rural, e para os dados adicionais nas notas fiscais de devoluções parciais

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function copiarTexto() {
+function copiarTextoSarkis() {
     var txtSarkis = document.getElementById("txt-sarkis");
 
     txtSarkis.select();
@@ -6,7 +6,23 @@ function copiarTexto() {
 
     document.execCommand("copy");
 
-    var mensagem = document.getElementById("mensagem");
+    var mensagem = document.getElementById("mensagem-sarkis");
+    mensagem.style.display = "block";
+
+    setTimeout(function() {
+        mensagem.style.display = "none";
+    }, 2000);
+}
+
+function copiarTextoNFDevolucao() {
+    var txtDev = document.getElementById("txt-dev");
+
+    txtDev.select();
+    txtDev.setSelectionRange(0, 99999);
+
+    document.execCommand("copy");
+
+    var mensagem = document.getElementById("mensagem-nf-dev");
     mensagem.style.display = "block";
 
     setTimeout(function() {
